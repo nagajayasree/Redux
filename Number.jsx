ddb2de990676e3347090b12d0f496a8ba7ffb3d6
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-08-25 20:25:30 +0530

    Create Number.jsx

---
+++ Number.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { createStore } from "redux";
+
+class Number extends Component {
+  render() {
+    const counter = (state = 7, action) => {
+      switch (action.type) {
+        case "Add":
+          return state + 1;
+        case "Subs":
+          return state - 1;
+        default:
+          return state;
+      }
+    };
+
+    let store = createStore(counter);
+
+    // store.subscribe(() => console.log(store.getState()));
+    store.subscribe(() => console.log("Number is", store.getState()));
+
+    store.dispatch({ type: "Add" });
+    store.dispatch({ type: "Subs" });
+
+    let increment = () => {
+      //   store.getState() + 1;
+      //   store.dispatch({ type: "Add" });
+    };
+
+    let decrement = () => {
+      //   store.getState() - 1;
+      //   store.dispatch({ type: "Subs" });
+    };
+
+    return (
+      <>
+        <p>Display Number using Redux</p>
+        {/* <button onClick={decrement()}>-</button> */}
+        {store.getState()}
+        {/* <button onClick={increment()}>+</button> */}
+      </>
+    );
+  }
+}
+
+export default Number;
